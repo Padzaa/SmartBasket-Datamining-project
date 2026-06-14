ad87a7a09b15f1e7211017c4a69b8c1b97129d0f
Pavle Knežević <padza127@gmail.com>
2026-06-14 15:59:29 +0200

    first commit

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="market_basket_analytics",
+    name="Smart Basket Datamining",
     version="1.0.0",
-    description="State-of-the-art Market Basket Analysis with Multi-Industry Applications",
+    description="Smart Basket System for Datamining project ",
     packages=find_packages(),
     install_requires=[
         "pandas>=2.0.0",
